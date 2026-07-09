aac63864bf8a3a0b1fb6f3ae17b1de8f98ba02ae
Aleks Novozilovs <aleksandr.novozhilov@gmail.com>
2026-07-09 22:59:26 +0100

    while loop

---
+++ day6.py
@@ -0,0 +1,9 @@
+password = ""
+
+while password != "Aleks" and password != "Aviozen":
+    password = input("Enter password: ")
+
+if password == "Aleks":
+    print ("Welcome back, Aleks!")
+else:
+    print ("Welcome to Aviozen!")
