a337f117e5abc665a345ba0d6a2c8a60d5aa8656
Aleks Novozilovs <aleksandr.novozhilov@gmail.com>
2026-07-09 20:02:56 +0100

    Day 3: inputs, variables and if statement

---
+++ day3.py
@@ -0,0 +1,8 @@
+name = input("What is your name? ")
+project =  input("What project are you building? ")
+exp = input("How many years have you worked in IT? ")
+print (f"Hello {name}!")
+print (f"you've worked in IT for {exp} years.")
+if project == "Aviozen":
+    print (f"{project} That sounds like an AI project!")
+else: print ("Good luck with your project!")
