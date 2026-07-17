9e1fdbc1eb48f249e481a5203c8e6d9ef78fecfb
Aleks Novozilovs <aleksandr.novozhilov@gmail.com>
2026-07-17 18:53:05 +0100

    tuples

---
+++ day17.py
@@ -0,0 +1,8 @@
+languages = ("Python", "Java", "PowerShell", "Git", "Linux")
+print("My tech stuck")
+print("---------------")
+
+for language in languages:
+    print (language)
+    
+print(f"Total technologies: {len(languages)}")
